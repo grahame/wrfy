b9f0ea4e69241decdced8aaf3abc5c56f5b0f1ac
Grahame Bowland <grahame@angrygoats.net>
2016-06-08 13:44:28 +0800

    release 0.3.0
    lint setup.py

--- setup.py
+++ setup.py
@@ -3,19 +3,19 @@ from setuptools import setup, find_packages
 install_requires = ["docker-py==1.8.1", "progressbar2==3.5.0"]
 
 setup(
-    author = "Grahame Bowland",
-    author_email = "grahame@angrygoats.net",
-    description = "docker helper",
-    license = "GPL3",
-    keywords = "docker",
-    url = "https://github.com/grahame/wrfy",
-    name = "wrfy",
-    version = "0.2.0",
-    packages = find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
-    install_requires = install_requires,
-    entry_points = {
+    author="Grahame Bowland",
+    author_email="grahame@angrygoats.net",
+    description="docker helper",
+    license="GPL3",
+    keywords="docker",
+    url="https://github.com/grahame/wrfy",
+    name="wrfy",
+    version="0.3.0",
+    packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+    install_requires=install_requires,
+    entry_points={
         "console_scripts": [
-            "wrfy = wrfy.cli:main",
+            "wrfy=wrfy.cli:main",
         ],
     }
 )
