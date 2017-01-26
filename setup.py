0bd8fe26087b87a3db725957a6ee4a3cc385c20f
Adam Hunter <ahunter@ccg.murdoch.edu.au>
2017-01-26 13:04:57 +0800

    docker==2.0.2, catch API error on pull

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 if not sys.version_info[0] == 3:
     sys.exit("Sorry, this is a Python 3 utility")
 
-install_requires = ["docker-py==1.8.1", "progressbar2==3.5.0"]
+install_requires = ["docker==2.0.2", "progressbar2==3.5.0"]
 
 setup(
     author="Grahame Bowland",
